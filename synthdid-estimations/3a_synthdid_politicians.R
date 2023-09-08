93d3deb35c3be4e4d24dabcd72815f66efcf6655
Paul Balluff <paul.balluff@gmail.com>
2023-09-08 13:22:52 +0200

    better synthdid estimations and plots

--- synthdid-estimations/3a_synthdid_politicians.R
+++ synthdid-estimations/3a_synthdid_politicians.R
@@ -71,11 +71,16 @@ df <- df %>%
 # in the post-treatment period
 # (takes the value of 1 if the variable outlet = www.oe24.at
 # in the years 2016-2020, and 0 otherwise)
+
+treatment_year <- 2016
+
 df <- df %>%
-  mutate(treat = as.integer(outlet == "www.oe24.at" & year >= 2016))
+  mutate(treat = as.integer(outlet == "www.oe24.at" & year >= treatment_year))
 
 # synthdid package doesn't like tibbles
 df <- as.data.frame(df)
+
+results <- list()
 
 # Run estimations for each actor and save results
 for (a in actors) {
@@ -87,19 +92,44 @@ for (a in actors) {
   )
 
   tau_hat <- synthdid_estimate(setup$Y, setup$N0, setup$T0)
-  # se = sqrt(vcov(tau_hat, method='placebo'))
-  # sprintf('point estimate: %1.5f', tau_hat)
-  # sprintf('95%% CI (%1.2f, %1.2f)', tau_hat - 1.96 * se, tau_hat + 1.96 * se)
-  saveRDS(tau_hat, paste0("results/3a_synthdid_", a, "_total.RDS"))
+  se = sqrt(vcov(tau_hat, method='placebo'))
+  tval = as.numeric(tau_hat) / se # t-values based on asymptotic formula
+  pval = 2 * pnorm(-abs(tval)) # p-values based on asymptotic formula
+  
+  results[[a]] <- data.frame(actor = a, 
+                             tau_hat = as.numeric(tau_hat),
+                             se = se,
+                             tval = tval,
+                             pval = pval,
+                             lci = as.numeric(tau_hat) - 1.96 * se,
+                             uci = as.numeric(tau_hat) + 1.96 * se)
 }
+
+results <- dplyr::bind_rows(results)
+
+saveRDS(results, paste0("results/3a_synthdid_total.RDS"))
 
 # Additional checks:
 # run estimations only considering
 # every year after treatment separately
-for (j in 2016:2019) {
-  new_df <- df[(df$year <= 2015 | df$year == j), ]
+years <- 2016:2019
 
-  for (a in actors) {
+results <- list()
+
+for (a in actors) {
+
+  actor_results <- data.frame(year=years)
+  actor_results$actor <- a
+  actor_results$tau_hat <- NA # average treatment effect on the treated (ATET)
+  actor_results$standard_error <- NA # standard error
+  actor_results$t_value <- NA # t value
+  actor_results$p_value <- NA # Pr(>|t|)
+  actor_results$lci <- NA # lower confidence interval
+  actor_results$uci <- NA  # upper confidence interval
+
+  for (j in years) {
+    new_df <- df[(df$year < treatment_year | df$year == j), ]
+
     setup <- synthdid::panel.matrices(new_df,
       unit = "id",
       time = "year",
@@ -108,10 +138,26 @@ for (j in 2016:2019) {
     )
 
     tau_hat <- synthdid_estimate(setup$Y, setup$N0, setup$T0)
-    # se = sqrt(vcov(tau_hat, method='placebo'))
-    # sprintf('point estimate: %1.5f', tau_hat)
-    # sprintf('95%% CI (%1.2f, %1.2f)', tau_hat - 1.96 * se, tau_hat + 1.96 * se)
+    se = sqrt(vcov(tau_hat, method='placebo'))
+    tval = as.numeric(tau_hat) / se # t-values based on asymptotic formula
+    pval = 2 * pnorm(-abs(tval)) # p-values based on asymptotic formula
 
-    saveRDS(tau_hat, paste0("results/3a_synthdid_", a, "_", j, ".RDS"))
-  }
+    actor_results[actor_results$year == j, 'tau_hat'] <- as.numeric(tau_hat)
+    actor_results[actor_results$year == j, 'standard_error'] <- se
+    actor_results[actor_results$year == j, 't_value'] <- tval
+    actor_results[actor_results$year == j, 'p_value'] <- pval
+    actor_results[actor_results$year == j, 'lci'] <- as.numeric(tau_hat) - 1.96 * se
+    actor_results[actor_results$year == j, 'uci'] <- as.numeric(tau_hat) + 1.96 * se
+   }
+   results[[a]] <- actor_results
 }
+
+
+results <- dplyr::bind_rows(results)
+
+
+# drop years without estimation
+results <- results[!is.na(results$tau_hat), ]
+results$pre_treatment <- results$year < treatment_year
+
+saveRDS(results, file = "results/3a_synthdid_year.RDS")
--- synthdid-estimations/plots.r
+++ synthdid-estimations/plots.r
@@ -1,41 +1,67 @@
-library(synthdid)
+rm(list=ls( ))
+
+library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(viridis)
 
-actors <- c("Strache", "Kurz", "Mitterlehner")
+years <- 2013:2019
 
-for (a in actors) {
-    results <- readRDS(paste0("results/3a_synthdid_", a, "_total.RDS"))
+results_3a <- readRDS("results/3a_synthdid_year.RDS")
+
+results_3a$pre_treatment <- (results_3a$year < 2016)
+
+results_3a$`Treatment Period` <- factor(ifelse(results_3a$pre_treatment,
+                                               'Pre',
+                                               'Post'), levels = c('Pre', 'Post'))
+
+plot_3a <- results_3a |>
+  rename(ATET = tau_hat) |> 
+  ggplot(aes(x=year, y=ATET, 
+             color = `Treatment Period`, 
+             shape = `Treatment Period`)) +
+  # convoluted solution to plot confidence intervals
+  geom_segment(aes(xend = year, y = lci, yend = uci)) +
+  geom_segment(aes(xend = year + 0.1, x = year - 0.1, yend = lci, y = lci)) +
+  geom_segment(aes(xend = year + 0.1, x = year - 0.1, yend = uci, y = uci)) +
+  # actual point estimates
+  geom_point(size=2) +
+  # fix year scale
+  scale_x_continuous(breaks = years) +
+  # theme
+  theme_clean() +
+  theme(legend.position = "none",  # Remove the legend
+        panel.spacing = unit(2, "lines"),
+        plot.background = element_rect(color = NA)) +
+  geom_hline(yintercept = 0, linetype = 'dashed') +
+  # colors for publication
+  scale_color_viridis(discrete = TRUE, option = "magma", end = 0.6, direction = -1) +
+  # shapes corresponding to the previous graphs
+  scale_shape_manual(values=c(17,19)) +
+  # scale_color_grey(start = 0, end = 0.4) +
+  # show the comparison point
+  # geom_vline(xintercept = 2015.5, linetype = 'dotted') +
+  # facet against actor
+  facet_wrap(~actor)
 
 
-    p <- synthdid::synthdid_plot(results, overlay = 0.99) +
-        scale_alpha(range = c(0, 1), guide = "none") +
-        theme_clean() +
-        theme(
-            legend.position = "top",
-            plot.background = element_rect(color = NA)
-        ) +
-        scale_color_viridis(discrete = TRUE, option = "magma", end = 0.6) +
-        scale_fill_viridis(discrete = TRUE, option = "magma", end = 0.6) +
-        geom_vline(xintercept = 2015)
+ggsave("plots/3a_synthdid.png",
+       plot_3a,
+       device = "png",
+       width = 16,
+       height = 6,
+       units = "cm",
+       dpi = 300,
+       scale = 2
+)
 
-    ggsave(paste0("plots/3a_synthdid_", a, "_total.png"),
-        p,
-        device = "png",
-        width = 16,
-        height = 9,
-        units = "cm",
-        dpi = 300,
-        scale = 2
-    )
-    ggsave(paste0("plots/3a_synthdid_", a, "_total.pdf"),
-        p,
-        device = "pdf",
-        width = 16,
-        height = 9,
-        units = "cm",
-        dpi = 300,
-        scale = 2
-    )
-}
+
+ggsave("plots/3a_synthdid.pdf",
+       plot_3a,
+       device = "pdf",
+       width = 16,
+       height = 6,
+       units = "cm",
+       dpi = 300,
+       scale = 2
+)
